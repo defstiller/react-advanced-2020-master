4cdf2b5add705c3bdaa0c2839f46201c8dfb375e
defstiller <81063003+defstiller@users.noreply.github.com>
2022-03-21 21:36:22 -0500

    Fixed useState object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import UseStateArray from './tutorial/1-useState/setup/3-useState-array.js'
+import UseStateObject from './tutorial/1-useState/setup/4-useState-object'
 function App() {
   return (
     <div className='container'>
-      <UseStateArray />
+      <UseStateObject />
     </div>
   )
 }
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+  function changeMessage() {
+    setPerson({...person, message: "new message"})
+  }
+  return <>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.message}</h3>
+    <button onClick={changeMessage} className="btn">
+      change message
+    </button>
+  </>
 };
 
 export default UseStateObject;
