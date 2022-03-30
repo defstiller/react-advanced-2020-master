2874df403d74844c1fbf689d6cc5f106f9f2ca64
defstiller <81063003+defstiller@users.noreply.github.com>
2022-03-30 12:11:43 -0500

    Fixed Reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import UseRefBasics from './tutorial/5-useRef/setup/1-useRef-basics'
+import Setup from "./tutorial/6-useReducer/setup"
 function App() {
   return (
     <div className='container'>
       <React.StrictMode>
-      <UseRefBasics />
+      <Setup />
       </React.StrictMode>
     </div>
   )
